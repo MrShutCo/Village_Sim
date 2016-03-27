c87d03df0c3942eaf28d37aff9c89ce936432652
MrShutCo <tylersafaric99@gmail.com>
2016-03-26 23:59:12 -0400

    Added basic Sprite and State
    ^^^

---
+++ Village_Sim/Village_Sim/GameStates/PlayingState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Village_Sim.GameStates {
+    public class PlayingState : GameState {
+
+        public PlayingState(VillageSim game)
+            :base(game){
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch) {
+           
+        }
+
+        public override void Update(GameTime gameTime) {
+            
+        }
+    }
+}
---
+++ Village_Sim/Village_Sim/Sprites/Sprite.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Village_Sim.Sprites {
+    public class Sprite {
+
+        private readonly Texture2D _texture;
+        protected Rectangle MovementBounds;
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Vector2 Position { get; set; }
+        public Vector2 Velocity { get; set; }
+        public Rectangle BoundingBox { get { return new Rectangle((int)Position.X, (int)Position.Y, 32, 32); } }
+
+        public Sprite(Texture2D texture, Vector2 position, Rectangle movementBounds) {
+            _texture = texture;
+            Position = position;
+            MovementBounds = movementBounds;
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            spriteBatch.Draw(_texture, Position, Color.White);
+        }
+
+        public virtual void Update(GameTime gameTime) {
+
+        }
+
+    }
+}
