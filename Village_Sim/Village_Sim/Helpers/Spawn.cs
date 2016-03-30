5e65eea24366fb1f6676246e9ee5e0c907a736f2
Nic Felder <nicfelder@outlook.com>
2016-03-29 19:24:28 -0500

    Added in Spawn.cs

---
+++ Village_Sim/Village_Sim/Helpers/Spawn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Village_Sim.Sprites;
+
+namespace Village_Sim.Helpers
+{
+    public class Spawn
+    {
+        private List<Villager> villagers = new List<Villager>();
+
+        public void SpawnVillagers(Villager villager, int spawnCap)
+        {
+            for (int i = 0; i < spawnCap; i++)
+            {
+                villagers.Add(villager);
+            }
+        }
+    }
+}
--- Village_Sim/Village_Sim/Sprites/Villager.cs
+++ Village_Sim/Village_Sim/Sprites/Villager.cs
@@ -5,10 +5,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Village_Sim.Helpers;
 
 namespace Village_Sim.Sprites {
-    public class Villager : Sprite {
-
+    public class Villager : Sprite
+    {
+ 
         public Villager(Texture2D texture, Vector2 position, Rectangle movementBounds)
             : base(texture, position, movementBounds){
             Velocity = Vector2.Zero;
